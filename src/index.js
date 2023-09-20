01e3d81537f2c94a4ff45835b11d86350fda3ff5
martynenkovlad <108723639+martynenkovlad@users.noreply.github.com>
2023-09-21 00:07:51 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+  
+    if (num % 2 === 0 || num % 3 === 0) return false;
+  
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+  
+    return true;
+  }
+  
+  function filterPrimes(numbers) {
+    return numbers.filter(isPrime);
+  }
+  
+  const inputNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const primeNumbers = filterPrimes(inputNumbers);
+  console.log(primeNumbers); 
